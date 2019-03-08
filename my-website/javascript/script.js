a7da4481868c1ecdb9c966eab5535f3f9b24a7c3
Enzo Rossetto <enzojmrossetto@gmail.com>
2019-03-08 01:21:16 -0300

    Changed the 'collapsible' button to a div and added a different icon using Font Awesome. Added some small styles changes to it. Rewrote the script to be able to use the same function to expand/collapse the menu when you click on 'collapsible' or any nav list item.

--- my-website/javascript/script.js
+++ my-website/javascript/script.js
@@ -1,13 +1,20 @@
 window.onload = () => {
-    var coll = document.getElementsByClassName("collapsible")[0];
-    coll.onclick = function () {
-        var content = this.previousElementSibling;
+    const collapsible = document.getElementsByClassName('collapsible')[0];
+    const menuList = document.querySelectorAll('nav > ul >li');
+
+    const expandableMenu = () => {
+        var content = collapsible.previousElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
-            this.innerText = "˅";
+            collapsible.innerHTML = '<i class="fas fa-angle-down"></i>';
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
-            this.innerText = "˄";
+            collapsible.innerHTML = '<i class="fas fa-angle-up"></i>';
         }
     };
+
+    collapsible.onclick = expandableMenu;
+    if (window.innerWidth <= 768) {
+        menuList.forEach(li => li.onclick = expandableMenu)
+    }
 }
